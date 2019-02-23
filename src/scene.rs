975e277e8f865f0e825ff8677625ec2fd23bc879
Clément Sibille <clement.sibille@protonmail.com>
2019-02-23 23:04:04 +0100

    Make SceneNode::children() return mutable ref

--- src/scene.rs
+++ src/scene.rs
@@ -76,8 +76,8 @@ impl SceneNode {
     }
 
     /// Returns the children of the node
-    pub fn children(&self) -> &Vec<SceneNode> {
-        &self.children
+    pub fn children(&mut self) -> &mut Vec<SceneNode> {
+        &mut self.children
     }
     
 
